bdbcb2a01d648eff57448a47c5d95348c0ebc189
Anatoly <Anatoly-Vinnikov>
2017-04-13 22:29:21 +0300

    New start
    Assembly to MVC

---
+++ Blog2/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog2.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Creator { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int RatingGood { get; set; }
+        public int RatingBad { get; set; }
+    }
+}
